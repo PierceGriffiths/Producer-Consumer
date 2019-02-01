1ab1d5c4ee5178ccc8b7e50d5b4db490e862a005
Pierce Griffiths <pierceagriffiths@gmail.com>
2019-02-01 12:10:23 -0600

    Improved readability of printf statements and fixed a small memory leak related to the line buffers

--- src/logreader_threads.c
+++ src/logreader_threads.c
@@ -10,44 +10,47 @@ void* producer_log_reader(producerlog_thread_args *args){
     if(args->producerLog != NULL){
 	lineBuffer = calloc(lineBufferSize, sizeof(**lineBuffer));
 	if(lineBuffer == NULL){
-	    args->ret = -1;
+	    args->ret = 1;
 	    //Lock mutex before printing error messages to stdout to ensure that it isn't printed while the consumer log is being written to stdout
 	    pthread_mutex_lock(args->mutex);
-	    printf("Failed to allocate memory needed to read %s.\n", PRODUCER_LOG_FILENAME);
+	    printf("Failed to allocate memory needed to read "PRODUCER_LOG_FILENAME"\n");
 	    goto producer_log_exit;
 	}
     }
     else{
 	args->ret = 1;
 	pthread_mutex_lock(args->mutex);
-	printf("%s was not written to, so it will not be read\n", PRODUCER_LOG_FILENAME);
+	printf(PRODUCER_LOG_FILENAME" was not written to, so it will not be read\n");
 	goto producer_log_exit;
     }
     args->producerLog = fopen(PRODUCER_LOG_FILENAME, "r");
 
     if(args->producerLog == NULL){
-	args->ret = -1;
+	free(lineBuffer);
+	args->ret = 1;
 	pthread_mutex_lock(args->mutex);
-	printf("Unable to read from %s.\n", PRODUCER_LOG_FILENAME);
+	printf("Unable to read from "PRODUCER_LOG_FILENAME"\n");
 	goto producer_log_exit;
     }
     else{
 	if(getline(lineBuffer, &lineBufferSize, args->producerLog) == -1){
 	    fclose(args->producerLog);
+	    free(lineBuffer);
 	    args->ret = 1;
 	    pthread_mutex_lock(args->mutex);
-	    printf("%s is empty.\n", PRODUCER_LOG_FILENAME);
+	    printf(PRODUCER_LOG_FILENAME" is empty\n");
 	    goto producer_log_exit;
 	}
 	pthread_mutex_lock(args->mutex);
-	printf("Reading from %s:\n", PRODUCER_LOG_FILENAME);
+	printf("Reading from "PRODUCER_LOG_FILENAME":\n");
 	do{
 	    printf("%s", *lineBuffer);
 	}
 	while(getline(lineBuffer, &lineBufferSize, args->producerLog) != -1);
 
 	fclose(args->producerLog);
-	printf("End of %s.\n\n", PRODUCER_LOG_FILENAME);
+	free(lineBuffer);
+	printf("End of "PRODUCER_LOG_FILENAME"\n\n");
     }
     args->ret = 0;
 producer_log_exit:
@@ -62,44 +65,47 @@ void* consumer_log_reader(consumerlog_thread_args *args){
     if(args->consumerLog != NULL){
 	lineBuffer = calloc(lineBufferSize, sizeof(**lineBuffer));
 	if(lineBuffer == NULL){
-	    args->ret = -1;
+	    args->ret = 1;
 	    //Lock mutex before printing error messages to stdout to ensure that it isn't printed while the producer log is being written to stdout
 	    pthread_mutex_lock(args->mutex);
-	    printf("Failed to allocate memory needed to read %s.\n", CONSUMER_LOG_FILENAME);
+	    printf("Failed to allocate memory needed to read "CONSUMER_LOG_FILENAME"\n");
 	    goto consumer_log_exit;
 	}
     }
     else{
 	args->ret = 1;
 	pthread_mutex_lock(args->mutex);
-	printf("%s was not written to, so it will not be read\n", CONSUMER_LOG_FILENAME);
+	printf(CONSUMER_LOG_FILENAME" was not written to, so it will not be read\n");
 	goto consumer_log_exit;
     }
     args->consumerLog = fopen(CONSUMER_LOG_FILENAME, "r");
 
     if(args->consumerLog == NULL){
-	args->ret = -1;
+	free(lineBuffer);
+	args->ret = 1;
 	pthread_mutex_lock(args->mutex);
-	printf("Unable to read from %s.\n", CONSUMER_LOG_FILENAME);
+	printf("Unable to read from "CONSUMER_LOG_FILENAME"\n");
 	goto consumer_log_exit;
     }
     else{
 	if(getline(lineBuffer, &lineBufferSize, args->consumerLog) == -1){
 	    fclose(args->consumerLog);
+	    free(lineBuffer);
 	    args->ret = 1;
 	    pthread_mutex_lock(args->mutex);
-	    printf("%s is empty.\n", CONSUMER_LOG_FILENAME);
+	    printf(CONSUMER_LOG_FILENAME" is empty\n");
 	    goto consumer_log_exit;
 	}
 	pthread_mutex_lock(args->mutex);
-	printf("Reading from %s:\n", CONSUMER_LOG_FILENAME);
+	printf("Reading from "CONSUMER_LOG_FILENAME":\n");
 	do{
 	    printf("%s", *lineBuffer);
 	}
 	while(getline(lineBuffer, &lineBufferSize, args->consumerLog) != -1);
 
 	fclose(args->consumerLog);
-	printf("End of %s.\n\n", CONSUMER_LOG_FILENAME);
+	free(lineBuffer);
+	printf("End of "CONSUMER_LOG_FILENAME"\n\n");
     }
     args->ret = 0;
 consumer_log_exit:
